26caa31e9cef6337a05b9f75c14d67e594431e4c
Nathan Cooper <nathan.lb.cooper@outlook.com>
2016-09-16 22:28:44 +0200

    Move get passwords into own class and get the es6 Promise typings

---
+++ src/commonPasswords.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import {AxiosResponse} from 'axios';
+import {Promise} from 'es6-promise';
+
+const passwordFileLocation =
+    "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/" +
+    "10_million_password_list_top_100.txt";
+
+export class CommonPasswords {
+    public get(): Promise<string[]> {
+        return axios.get(passwordFileLocation)
+        .then(function (response) {
+            return response.data.split("\n");;
+        })
+        .catch(function (error) {
+            console.log(error);
+            return error;
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,15 @@
-import axios from 'axios';
+import {AxiosResponse} from 'axios';
+import {Promise} from 'es6-promise';
 
-class Greeter {
-    constructor() { }
-    greet(): any {
-        return axios.get('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_100.txt')
-        .then(function (response) {
-            return response;
+import { CommonPasswords } from './commonPasswords';
+
+class Main {
+    public run() {
+        let passwords = new CommonPasswords();
+        passwords.get().then(response => {
+            document.body.innerHTML = JSON.stringify(response);
         })
-        .catch(function (error) {
-            console.log(error);
-            return error;
-        });
     }
 };
 
-var greeter = new Greeter();
-    
-greeter.greet().then(function(response) {
-    document.body.innerHTML = response.data;
-})
+new Main().run();
