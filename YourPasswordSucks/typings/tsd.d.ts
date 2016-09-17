efa252454b0b6a28df4fb66757acba92378f5e6d
Nathan Cooper <nathan.lb.cooper@outlook.com>
2016-09-17 19:41:20 +0200

    Reorg folders, install tests: Mocha, Chai, Sinon, Karma

---
+++ YourPasswordSucks/typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="assertion-error/assertion-error.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="sinon/sinon.d.ts" />
